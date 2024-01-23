2cb21ec8fd7a835178a560601d9eeb886ff12363
CarEsteban <car23016@uvg.edu.gt>
2024-01-22 19:13:48 -0600

    test frequency

--- test/RadioTest.java
+++ test/RadioTest.java
@@ -5,24 +5,53 @@ import org.junit.Test;
 
 
 public class RadioTest {
-    @Test
-    public void testGetCurrentFrequency() {
-
-    }
-
+    IRadio radio =  new Radio();
 
     @Test
     public void testGetStateFalse() {
-
-        IRadio radio =  new Radio();
 
         assertEquals(false, radio.getState());
         
     }
 
     @Test
-    public void testGetStateAMFM() {
+    public void testTooglePowerOffOn() {
+        //inicia como false
+        radio.tooglePowerOffOn();
+        //cambia a true
+        assertEquals(true, radio.getState());
+        
+    }
 
+    @Test
+    public void testToogleAMFM() {
+        //inicia en FM
+        radio.toogleAMFM();
+        //cambia a AM
+        assertEquals(true, radio.getStateAMFM());
+
+    }
+    @Test
+    public void testGetStateFM() {
+        //al ser falso, es FM
+        assertEquals(false, radio.getStateAMFM());
+    }
+    @Test
+    public void testGetStateAM() {
+        //al ser falso, es FM
+        radio.toogleAMFM();
+        assertEquals(true, radio.getStateAMFM());
+    }
+
+    @Test
+    public void testGetCurrentFrequencyFM() {
+        assertEquals(87.9f, radio.getCurrentFrequency(),0.001);
+    }
+
+    @Test
+    public void testGetCurrentFrequencyAM() {
+        radio.toogleAMFM();
+        assertEquals(530, radio.getCurrentFrequency(),0.1);
     }
 
     @Test
@@ -40,18 +69,5 @@ public class RadioTest {
 
     }
 
-    @Test
-    public void testToogleAMFM() {
 
-    }
-
-    @Test
-    public void testTooglePowerOffOn() {
-        IRadio radio =  new Radio();
-        //inicia como false
-        radio.tooglePowerOffOn();
-        //cambia a true
-        assertEquals(true, radio.getState());
-        
-    }
 }

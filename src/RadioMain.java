f20fd340b13021be31f4cc8cd5f5652d97ec0ff2
Samuel Mejía <150869146+Biggles-insano@users.noreply.github.com>
2024-01-23 00:25:15 -0600

    Update RadioMain.java

--- src/RadioMain.java
+++ src/RadioMain.java
@@ -5,95 +5,89 @@ public class RadioMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         IRadio radio = new Radio();  // Instancia de la clase Radio
-        boolean continuar=true, subContinuar=true;
-        int opcion,button;
+        boolean continuar = true, subContinuar = true;
+        int opcion, button;
 
         while (continuar) {
             System.out.println("Menú:");
             System.out.println("1. Prende el radio");
             System.out.println("2. Salir");
             System.out.print("Ingresa la opción: ");
-    
+
             opcion = scanner.nextInt();
-    
+
             switch (opcion) {
                 case 1:
                     radio.tooglePowerOffOn();
                     System.out.println("Estado del radio: " + (radio.getState() ? "Encendido" : "Apagado"));
-                    subContinuar=true;
+                    subContinuar = true;
                     while (subContinuar) {
                         System.out.println("1. Cambia de AM a FM a AM");
                         System.out.println("2. Avanzar en el dial de las emisoras. Al llegar al final del dial inicia nuevamente.");
-                        System.out.println("3. Guardar la emisora actual en favoritos");
-                        System.out.println("4. Seleccionar una emisora favorita");
-                        System.out.println("5. Apagar radio");
-                        
+                        System.out.println("3. Retroceder en el dial de las emisoras. Al llegar al inicio del dial inicia nuevamente.");
+                        System.out.println("4. Guardar la emisora actual en favoritos");
+                        System.out.println("5. Seleccionar una emisora favorita");
+                        System.out.println("6. Apagar radio");
+
                         opcion = scanner.nextInt();
                         scanner.nextLine();
-        
+
                         switch (opcion) {
                             case 1:
-                                
-                    //radio.toogleAMFM();
-                                subContinuar = volverAlMenu(scanner, " al menú? ",radio);
+                                radio.toogleAMFM();
+                                System.out.println("Banda actual: " + (radio.getStateAMFM() ? "AM" : "FM"));
+                                subContinuar = volverAlMenu(scanner, " al menú? ", radio);
                                 break;
-                    
+
                             case 2:
-                            
-                    //     radio.nextFrequency();
-                    //     System.out.println("Frecuencia actual: " + radio.getCurrentFrequency());
-                    //     break;
-                                subContinuar = volverAlMenu(scanner, " al menú? ",radio);
+                                radio.nextFrequency();
+                                System.out.println("Frecuencia actual: " + radio.getCurrentFrequency());
                                 break;
-                    
+
                             case 3:
+                                radio.previousFrequency();
+                                System.out.println("Frecuencia actual: " + radio.getCurrentFrequency());
+                                break;
+
+                            case 4:
                                 System.out.println("Usted se encuentra en: " + (radio.getStateAMFM() ? "AM" : "FM"));
-                                System.out.println("Frecuencia actual: " + radio.getCurrentFrequency());    
-        
+                                System.out.println("Frecuencia actual: " + radio.getCurrentFrequency());
+
                                 System.out.println("Seleccione el boton para guardar en favoritos:");
                                 button = scanner.nextInt();
                                 scanner.nextLine();
-        
+
                                 radio.setFavFrequency(button);
-        
+
                                 System.out.println("Frecuencia " + radio.getFavFrequency(button) + " guardada en el boton: " + button);
-        
-                                subContinuar = volverAlMenu(scanner, " al menú? ",radio);
-        
-        
-        
+
+                                subContinuar = volverAlMenu(scanner, " al menú? ", radio);
                                 break;
-        
-                            case 4:
+
+                            case 5:
                                 System.out.println("Seleccione el boton de la frecuencia favorita");
                                 button = scanner.nextInt();
                                 scanner.nextLine();
 
                                 System.out.println("La frecuencia es: " + radio.getFavFrequency(button));
 
-                            
-                                subContinuar = volverAlMenu(scanner, " al menú? ",radio);
+                                subContinuar = volverAlMenu(scanner, " al menú? ", radio);
                                 break;
-                        
-                            case 5:                
+
+                            case 6:
                                 System.out.println("Saliendo del programa...");
-        
+                                subContinuar = false;
                                 break;
-                            default:                
+                            default:
                                 System.out.println("Opción no válida. Inténtalo de nuevo.");
-        
                                 break;
                         }
                     }
-    
-    
-                    
                     break;
                 case 2:
                     System.out.println("Saliendo del programa...");
-                    continuar=false;
+                    continuar = false;
                     break;
-    
             }
         }
         scanner.close();
@@ -107,7 +101,6 @@ public class RadioMain {
             System.out.println("Apagando la radio...");
             radio.tooglePowerOffOn();
             return false;
-            
         } else {
             return true;
         }
